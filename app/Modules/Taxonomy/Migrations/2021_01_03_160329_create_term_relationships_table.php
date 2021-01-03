4689c966b2e6bc51dcff0fbb0ca01860c13cdee6
Phuthuyxam <48133351+Phuthuyxam@users.noreply.github.com>
2021-01-03 23:43:19 +0700

    add migrations post module

---
+++ app/Modules/Taxonomy/Migrations/2021_01_03_160329_create_term_relationships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermRelationshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('term_relationships', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('term_relationships');
+    }
+}
