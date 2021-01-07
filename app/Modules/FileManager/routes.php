150d3bf9e053b464793ddba3a9db55d67ff33827
phuthuyxam <phamphuc14111997@gmail.com>
2021-01-07 09:12:55 +0700

    fix sidebar file manager

--- app/Modules/FileManager/routes.php
+++ app/Modules/FileManager/routes.php
@@ -10,7 +10,7 @@ Route::group(
         ]);
 
         Route::get('/file-manager-ckeditor', [
-            'as' => 'index',
+            'as' => 'test-ck-editor',
             'uses' => 'FileManagerController@cktest'
         ]);
 
